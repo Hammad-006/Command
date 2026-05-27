6f151deb47b69d351539f9d5aff79ffac5440f36
Hammad <hammadmahmood006@gmail.com>
2026-05-27 21:05:34 +0530

    Added footer/Added loading Screen for home pg only/ other minor updates and clean-ups

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+/* window.addEventListener("load", function () {
+  const loader = document.querySelector(".loadingScr");
+
+  // Fade out
+  loader.classList.add("hideLoadingScr");
+
+  // Remove from DOM after fade completes
+  loader.addEventListener("transitionend", function () {
+    loader.remove();
+  });
+});
+ */
