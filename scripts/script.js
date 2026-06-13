7112aa408aa8a70c19049e87cb3798ec28b5eb98
Hammad <hammadmahmood006@gmail.com>
2026-06-13 15:04:35 +0530

    Add features: tokenizer,clear button | UI improvments | code clean up for home page

--- scripts/script.js
+++ scripts/script.js
@@ -8,6 +8,18 @@ const opt = document.querySelector(".opt");
   console.log(` Start: ${input.selectionStart} ||| End: ${input.selectionEnd}`);
 }); */
 
+/* ^^^ clear button hide and show^^^ */
+const clrbtn = document.querySelector(".clearBtn");
+input.addEventListener("input", () => {
+  clrbtn.style.display = input.value.trim() ? "block" : "none";
+});
+
+clrbtn.addEventListener("click", () => {
+  input.value = ``;
+  clrbtn.style.display = "none";
+  input.focus();
+});
+
 /* ^^^^^^^Inser and Cursor reposition^^^^^^^^^^^^ */
 
 repositionCursor = (currentPos, offset) => {
@@ -69,21 +81,55 @@ input.addEventListener("input", (e) => {
   }
 });
 
-/* ^^^^^^^^^^^^^^^^^ */
+/* ^^^^^^^Error Handling Logic^^^^^^^^ */
+const showError = (err) => {
+  const errorMsg = document.querySelector(".errorMsg");
+  errorMsg.textContent = msg;
+  errorMsg.classList.add("showError");
+};
+
+const hideError = () => {
+  document.querySelector(".errorMsg").classList.remove("showError");
+};
+
+/* ^^^^^^Extracting the Variables form the input^^^^^^^^ */
 
 function getVars(formula) {
   const vars = new Set();
   for (const char of formula) {
-    if (/[A-za-z]/.test(char)) vars.add(char);
+    if (/[A-Za-z]/.test(char)) vars.add(char);
   }
   console.log([...vars]);
   return [...vars].sort();
 }
 
+/* ^^^^^^ */
+// Note: here we have used for-of loop and it works fine for now. But if we need to access index to check the next chars then we need a while loop with manual updating of i.
+const tokenizer = (formula) => {
+  let tokens = [];
+  for (const ch of formula) {
+    if (ch == " ") continue;
+    if (ch === /[A-Za-z]/)
+      tokens.push({ type: "VAR", val: ch }); // VAR not var
+    else if (ch === "¬") tokens.push({ type: "NOT" });
+    else if (ch === "∧") tokens.push({ type: "AND" });
+    else if (ch === "∨") tokens.push({ type: "OR" });
+    else if (ch === "→") tokens.push({ type: "IF" });
+    else if (ch === "↔") tokens.push({ type: "IFF" });
+    else if (ch === "(") tokens.push({ type: "LPAREN" });
+    else if (ch === ")") tokens.push({ type: "RPAREN" });
+    else throw new Error(`Invalid character/Symbol: ${ch}`);
+  }
+  return tokens;
+};
+
 /* ^^^^^^^^Generate^^^^^^^^^ */
 
 const generateTruthTable = () => {
-  const val = input.value;
+  hideError();
+  const rawval = input.value.trim();
   console.log(`User Input: ${input.value}`);
-  getVars(val);
+  const vars = getVars(rawval);
 };
+
+/* ^^^^^^^^ */
