d3364835f5a19b2573c7591d7f9929f12af7d569
Hammad <hammadmahmood006@gmail.com>
2026-06-13 21:23:37 +0530

    Add parser(tokens) [parses the tokens and returns a tree for the input formula]

--- scripts/script.js
+++ scripts/script.js
@@ -33,6 +33,7 @@ insertSym = (sym) => {
   let offset = sym.length;
   repositionCursor(start, offset);
   input.focus();
+  input.dispatchEvent(new Event("input", { bubbles: true }));
 };
 
 /* ^^^^^^^ */
@@ -103,7 +104,7 @@ function getVars(formula) {
   return [...vars].sort();
 }
 
-/* ^^^^^^ */
+/* ^^^Generating Tokens for the formula^^ */
 // Note: here we have used for-of loop and it works fine for now. But if we need to access index to check the next chars then we need a while loop with manual updating of i.
 const tokenizer = (formula) => {
   let tokens = [];
@@ -121,6 +122,107 @@ const tokenizer = (formula) => {
     else throw new Error(`Invalid character/Symbol: ${ch}`);
   }
   return tokens;
+};
+
+/* ^^^^^ Parseing adn tree building Logic ^^^^^ */
+const parse = (tokens) => {
+  let i = 0;
+  const peek = () => {
+    return tokens[i];
+  };
+
+  const eat = (type) => {
+    /*  genral form:
+    function consume(expectedType) {
+        const token = peek();
+        if (token.type !== expectedType)
+            throw Error("Wrong token");
+        pos++;
+        return token; 
+    } */
+    const t = peek();
+    if (type && t?.type !== type)
+      throw new Error(`Eat shit cuz what's this: ${type}`);
+    i++;
+    //here we are not returning anything, cuz we dont need to.
+  };
+
+  //Ordered from loosest to tightest puh: IFF → IF → OR → AND → NOT → ATOM
+
+  function parseIFF() {
+    let left = parseIF();
+    while (peek()?.type === "IFF") {
+      eat("IFF");
+      let right = parseNOT();
+      left = { type: "IFF", left, right };
+    }
+    return left;
+  }
+
+  function parseIF() {
+    let left = parseOR();
+    while (peek()?.type() === "IF") {
+      eat("IF");
+      let right = parseNOT();
+      left = { type: "IF", left, right };
+    }
+    return left;
+  }
+
+  function parseOR() {
+    let left = parseAND();
+    while (peek()?.type === "OR") {
+      eat("OR");
+      let right = parseNOT();
+      left = { type: "OR", left, right };
+    }
+    return left;
+  }
+
+  function parseAND() {
+    let left = parseNOT();
+    while (peek()?.type === "AND") {
+      eat("AND");
+      let right = parseNOT();
+      left = { type: "AND", left, right };
+    }
+    return left;
+  }
+
+  function parseNOT() {
+    if (peek()?.type === "NOT") {
+      eat("NOT");
+      let operand = parseNOT();
+      return { type: "NOT", val: operand };
+    }
+    return parseATOM();
+  }
+
+  function parseATOM() {
+    const t = peek();
+    if (!t)
+      throw new Error(
+        `Fuck up at [parseATOM]. Incomplete Expression prolly, check the end of the formula`,
+      );
+    if (t.type === `VAR`) {
+      eat("VAR");
+      return { type: "VAR", val: t.val };
+    }
+    if (t.type === `LPAREN`) {
+      eat("LPAREN");
+      const subExpression = parseIFF();
+      eat("RPAREN");
+      return subExpression;
+    }
+    throw new Error(`Atom (character) not valid somewhere in the input`);
+  }
+
+  const tree = parseIFF();
+
+  if (i < tokens.length)
+    throw new Error(`not able to parse beyond ${token[i]}`);
+
+  return tree;
 };
 
 /* ^^^^^^^^Generate^^^^^^^^^ */
